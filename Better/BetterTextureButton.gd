0ed4c509473fb8b3c0397d2b408fcdc3b1760481
RattusInVitaReali <46037351+RattusInVitaReali@users.noreply.github.com>
2022-11-23 02:40:05 +0100

    better button

---
+++ Better/BetterTextureButton.gd
@@ -0,0 +1,10 @@
+extends TextureButton
+class_name BetterTextureButton
+
+export (AudioStream) var button_sound = preload("res://RESOURCES/Sound/ButtonSound.wav")
+
+func _ready():
+	$AudioStreamPlayer.stream = button_sound
+
+func _on_BetterTextureButton_pressed() -> void:
+	$AudioStreamPlayer.play()
